2073f4f30010af3ffe477c2de2cfdf7a76bdef16
Devina Gillis <devinagillis0@gmail.com>
2023-09-25 16:01:34 -0700

    updated file organization and added route handlers

--- src/app.js
+++ src/app.js
@@ -1,51 +1,12 @@
 const express = require("express");
 const app = express();
-
+const pasteRouter = require("./pastes/pastes.router");
 // TODO: Follow instructions in the checkpoint to implement ths API.
 const pastes = require("./data/pastes-data");
 app.use(express.json());
-app.use("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params;
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
 
-  if (foundPaste) {
-    res.json({ data: foundPaste });
-  } else {
-    next({ status: 404, message: `Paste id not found: ${pasteId}` });
-  }
-});
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next(); // Call `next()` without an error message if the result exists
-  }
-  next({
-    status: 400,
-    message: "A `text` property is required"
-  });
-}
+app.use("/pastes", pasteRouter);
 
-app.get("/pastes", (req, res) => {
-  res.json({ data: pastes });
-});
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-app.post("/pastes", bodyHasTextProperty, (req, res) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-  //  may look a bit strange, but it is still standard destructuring. This way, if the body doesn't contain a data property, 
-  // the destructuring will still succeed because you have supplied a default value of {} for the data property.
- 
-  const newPaste = {
-    id: ++lastPasteId, // Increment last ID, then assign as the current ID
-    name,
-    syntax,
-    exposure,
-    expiration,
-    text,
-    user_id,
-  };
-  pastes.push(newPaste);
-  res.status(201).json({ data: newPaste });
-});
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
